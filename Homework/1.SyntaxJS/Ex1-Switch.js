454ec7f1b78d81eee0ea09b1a98d20b33de4c17a
Valentin-1 <valikv03@gmail.com>
2022-02-03 12:31:58 +0200

    Homework-ex1

---
+++ Homework/1.SyntaxJS/Ex1-Switch.js
@@ -0,0 +1,35 @@
+let number = prompt('Introduceti numarul')
+switch (number) {
+    case '0':
+        alert('Zero');
+        break;
+    case '1':
+        alert('Unu');
+        break;
+    case '2':
+        alert('Doi');
+        break;
+    case '3':
+        alert('Trei');
+        break;
+    case '4':
+        alert('Patru');
+        break;
+    case '5':
+        alert('Cinci');
+        break;
+    case '6':
+        alert('Sase');
+        break;
+    case '7':
+        alert('Sapte');
+        break;
+    case '8':
+        alert('Opt');
+        break;
+    case '9':
+        alert('Noua');
+        break;
+    default:
+        alert('Numarul nu a fost introdus corect');
+}
