959f11d29b4d4437b9886c8c7c15667f3d75cff9
Valentin-1 <valikv03@gmail.com>
2022-02-03 12:46:23 +0200

    Homework-Ex2

---
+++ Homework/1.SyntaxJS/Ex2.js
@@ -0,0 +1,12 @@
+a = prompt('Introduceti primul numar din interval');
+b = prompt('Introduceti al doilea numar din interval');
+
+let sum = 0;
+
+for (let i = a; i <= b; ++i) {
+    if (i % 2 == 0) {
+        sum = i + sum;
+    }
+}
+
+console.log('Suma este = ' + sum);
