468501514478a215fbc9b07e72a201956bda4f82
Valentin-1 <valikv03@gmail.com>
2022-02-08 19:38:01 +0200

    Homework-ex9

---
+++ Homework/1.SyntaxJS/Ex9-UserInteraction.js
@@ -0,0 +1,4 @@
+let userName = prompt('Introduceti numele');
+alert(`Salut ${userName}!`);
+
+let answer = (confirm('Cunoașteți limbajul Js?')) ? alert('Bravo!') : alert('Spoate de inceput chiar acum!');
