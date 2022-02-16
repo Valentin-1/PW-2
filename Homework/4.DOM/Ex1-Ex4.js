2af770312657b2b245982192d75ad9ad904356b2
Valentin-1 <valikv03@gmail.com>
2022-02-16 12:50:16 +0200

    HomeW(DOM)-ex1-ex3

---
+++ Homework/4.DOM/Ex1-Ex4.js
@@ -0,0 +1,13 @@
+document.body.style.background = "red";
+
+setTimeout(function background() {
+    document.body.style.background = "green";
+}, 3000);
+
+
+function buttonClickColor() {
+    document.body.style.background = "orange";
+}
+
+let list = document.getElementById("list");
+
