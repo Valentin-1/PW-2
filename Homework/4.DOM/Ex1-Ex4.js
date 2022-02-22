846f83eb727ed4cce6f51ca7f2a70f94e8cde296
Valentin-1 <valikv03@gmail.com>
2022-02-22 21:24:56 +0200

    HomeW(DOM)-ex4

--- Homework/4.DOM/Ex1-Ex4.js
+++ Homework/4.DOM/Ex1-Ex4.js
@@ -1,4 +1,4 @@
-document.body.style.background = "red";
+/*document.body.style.background = "red";
 
 setTimeout(function background() {
     document.body.style.background = "green";
@@ -7,7 +7,25 @@ setTimeout(function background() {
 
 function buttonClickColor() {
     document.body.style.background = "orange";
-}
+}*/
+
+const listItems = document.getElementsByTagName("li"); 
+
+let i = 1;
+setInterval (() => {
+    if (i <= 20){
+        const list = document.createElement("li");
+        const textList = document.createTextNode(`Element ${i}`);
+        list.appendChild(textList);
+        document.getElementById("listID").appendChild(list);
+        if (i % 2 === 0) {
+            list.style.backgroundColor = "green"; 
+        } else {
+            list.style.backgroundColor = "purple";
+        }
+        i++;
+    }
+}, 2000);
 
 let list = document.getElementById("list");
 
